61984f46392973038bc413579ac0ff393fe7edc6
Tomasz Smolarczyk <smolarczyk.thomas@gmail.com>
2023-04-20 07:32:55 +0200

    Hero component css

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,20 +1,24 @@
-import React from "react";
-import { Container } from "../Header/styles";
+import { Container } from "./styles";
 import ScrollAnimation from "react-animate-on-scroll";
-import "animate.css/animate.min.css";
 
 const Hero = () => {
   return (
     <Container id="home">
       <div className="hero-text">
-        <ScrollAnimation animateIn="animate__fadeIn">
+        <ScrollAnimation animateIn="fadeInUp">
           <p>elo</p>
         </ScrollAnimation>
-        <ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInUp" delay={0.2 * 1000}>
           <h1>Tom Smolarczyk</h1>
         </ScrollAnimation>
         <ScrollAnimation>jestem front</ScrollAnimation>
         <ScrollAnimation>pracuje w react </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInUp" delay={0.6 * 1000}>
+          <p className="small-resume">
+            Sou um técnico em informática com foco em desenvolvimento Web com
+            JavaScript.
+          </p>
+        </ScrollAnimation>
       </div>
     </Container>
   );
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { Container } from "../Header/styles";
 import Hero from "../Hero/Hero";
+import { Container } from "./styles";
 
 const Main = () => {
   return (
     <>
       <Container>
+        <Hero />
+        <Hero />
+        <Hero />
         <Hero />
       </Container>
     </>
